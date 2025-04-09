a3e811270939485f8b3edfee116735c68ebdc436
Mamo YZ <yzmamo@gmail.com>
2025-04-09 14:12:04 +0300

    chore: added homepage translations

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
 	<provet-stack class="n-color-status-info n-padding-b-xxl" align-items="center" justify-content="center" style="height: 100vh">
-		<provet-stack direction="vertical" align-items="center" gap="l">
+		<provet-stack class="n-container-l" direction="vertical" align-items="center" gap="l">
 			<AppLogo style="width: 300px" />
-			<h1 class="n-color-text-on-accent n-align-center n n-font-size-xxxl n-font-weight-heading">{{ $t("auth.header.title") }}</h1>
+			<h1 class="n-color-text-on-accent n-align-center n n-font-size-xxxl n-font-weight-heading">{{ $t("home.title") }}</h1>
 			<div class="n-container-xs n-margin-i-auto n-align-center">
-				<h2 class="n-color-text-on-accent n-align-center n n-font-weight-active">{{ $t("auth.header.description") }}</h2>
+				<h2 class="n-color-text-on-accent n-align-center n n-font-weight-active">{{ $t("home.description") }}</h2>
 			</div>
 			<NuxtLink to="signup">
-				<provet-button style="--n-button-background-color: var(--n-color-border-warning)" size="l">Get Started Today</provet-button>
+				<provet-button style="--n-button-background-color: var(--n-color-border-warning)" size="l">{{ $t("home.cta_label") }}</provet-button>
 			</NuxtLink>
 		</provet-stack>
 	</provet-stack>
